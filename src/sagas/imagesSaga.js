09747319b1212c4be35d445aa6e366b1a133c1ff
ugrylmz <uguryilmaz235@gmail.com>
2020-12-11 19:19:57 +0300

    adding saga to project

---
+++ src/sagas/imagesSaga.js
@@ -0,0 +1,22 @@
+import { select, takeEvery, call, put } from 'redux-saga/effects';
+import { IMAGES } from '../constants';
+import { fetchImages } from '../api';
+import { setImages, setError } from '../actions';
+
+const getPage = state => state.nextPage;
+
+function* handleImagesLoad() {
+    try {
+        const page = yield select(getPage);
+        const images = yield call(fetchImages, page);
+        yield put(setImages(images));
+
+    } catch (error) {
+        yield put(setError(error));
+    }
+
+}
+
+export default function* watchImagesLoad() {
+    yield takeEvery(IMAGES.LOAD, handleImagesLoad);
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
-
+import rootSaga from '../sagas';
+import createSagaMiddleware from 'redux-saga';
 const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
     const store = createStore(
         rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__(),
+        compose(
+            applyMiddleware(sagaMiddleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                window.__REDUX_DEVTOOLS_EXTENSION__(),
+        ),
     );
+    sagaMiddleware.run(rootSaga);
+
+    store.dispatch({ type: 'DANG' });
     return store;
 };
 
